c561913f61b92611b9e5a1f06e6eeeb9e9a6dd3b
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-08-27 21:58:52 +0700

    feat(packages/token_provider): init package token provider

---
+++ packages/token_provider/lib/src/token_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class TokenProvider {
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
+
+  Future<String?> getToken() async {
+    return await _storage.read(key: 'auth_token');
+  }
+
+  Future<void> setToken(String token) async {
+    await _storage.write(key: 'auth_token', value: token);
+  }
+
+  Future<void> clearToken() async {
+    await _storage.delete(key: 'auth_token');
+  }
+}
