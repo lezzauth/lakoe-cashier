be22d2e55ccbfb3ebbcadeb2e5e1ef1154d71840
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-09-16 17:24:47 +0700

    spacing at ProfileCard

--- lib/features/account/presentation/screens/account_master.dart
+++ lib/features/account/presentation/screens/account_master.dart
@@ -131,7 +131,7 @@ class ProfileCard extends StatelessWidget {
         clipBehavior: Clip.none,
         children: [
           Positioned(
-            bottom: -36,
+            bottom: -40,
             left: 0,
             right: 0,
             child: Container(
