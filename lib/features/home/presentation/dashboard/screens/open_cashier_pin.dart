c2df189aa00a700fb1f47952da80b975c2e94ae8
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-09-18 12:11:32 +0700

    message wrong PIN

--- lib/features/home/presentation/dashboard/screens/open_cashier_pin.dart
+++ lib/features/home/presentation/dashboard/screens/open_cashier_pin.dart
@@ -1,6 +1,7 @@
 import 'package:cashier_repository/cashier_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:point_of_sales_cashier/common/widgets/form/dotted_pin.dart';
 import 'package:point_of_sales_cashier/common/widgets/form/number_pad.dart';
 import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_body_s.dart';
@@ -11,6 +12,7 @@ import 'package:point_of_sales_cashier/features/cashier/application/cubit/cashie
 import 'package:point_of_sales_cashier/features/cashier/application/cubit/cashier/cashier_state.dart';
 import 'package:point_of_sales_cashier/features/home/data/arguments/open_cashier_pin_argument.dart';
 import 'package:point_of_sales_cashier/utils/constants/colors.dart';
+import 'package:point_of_sales_cashier/utils/constants/sizes.dart';
 
 class OpenCashierPinScreen extends StatefulWidget {
   const OpenCashierPinScreen({super.key});
@@ -21,6 +23,9 @@ class OpenCashierPinScreen extends StatefulWidget {
 
 class _OpenCashierPinScreenState extends State<OpenCashierPinScreen> {
   final TextEditingController _pinController = TextEditingController();
+
+  bool checkPIN = false;
+  bool isPinWrong = false;
 
   onOpenCashierInitial(String value, int initialBalance) {
     AuthState authState = context.read<AuthCubit>().state;
@@ -54,7 +59,16 @@ class _OpenCashierPinScreenState extends State<OpenCashierPinScreen> {
 
     return BlocListener<CashierCubit, CashierState>(
       listener: (context, state) {
-        if (state is CashierOpened) {
+        if (state is CashierOpenInProgress) {
+          setState(() {
+            checkPIN = true;
+          });
+        } else if (state is CashierOpenFailure) {
+          _pinController.clear();
+          setState(() {
+            isPinWrong = true;
+          });
+        } else if (state is CashierOpened) {
           Navigator.popAndPushNamed(context, "/cashier/explore-products");
         }
       },
@@ -68,7 +82,6 @@ class _OpenCashierPinScreenState extends State<OpenCashierPinScreen> {
                 child: SingleChildScrollView(
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       Container(
                         margin: const EdgeInsets.only(bottom: 40, top: 56),
@@ -87,19 +100,45 @@ class _OpenCashierPinScreenState extends State<OpenCashierPinScreen> {
                           ],
                         ),
                       ),
-                      DottedPin(
-                        length: 4,
-                        controller: _pinController,
-                        onCompleted: (value) {
-                          if (arguments is OpenCashierInitial) {
-                            onOpenCashierInitial(
-                                value, arguments.initialBalance);
-                          }
-                          if (arguments is OpenCashierReInitial) {
-                            onOpenCashierReInitial(value);
-                          }
-                        },
-                      ),
+                      if (checkPIN && !isPinWrong)
+                        const SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(TColors.primary),
+                          ),
+                        ),
+                      if (!checkPIN || isPinWrong)
+                        DottedPin(
+                          length: 4,
+                          controller: _pinController,
+                          onCompleted: (value) {
+                            setState(() {
+                              checkPIN = false;
+                              isPinWrong = false;
+                            });
+                            if (arguments is OpenCashierInitial) {
+                              onOpenCashierInitial(
+                                  value, arguments.initialBalance);
+                            }
+                            if (arguments is OpenCashierReInitial) {
+                              onOpenCashierReInitial(value);
+                            }
+                          },
+                        ),
+                      if (isPinWrong)
+                        Container(
+                          margin: const EdgeInsets.only(top: 12),
+                          child: Text(
+                            "PIN Salah. Coba Lagi.",
+                            style: GoogleFonts.inter(
+                              fontSize: TSizes.fontSizeBodyS,
+                              color: TColors.error,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        )
                     ],
                   ),
                 ),
