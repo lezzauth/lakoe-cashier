8a6bf33dd45c5ea389dbdfb012cd29ea43d51028
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-09-19 21:31:06 +0700

    general bottomsheet

--- lib/features/account/presentation/screens/form/email_edit.dart
+++ lib/features/account/presentation/screens/form/email_edit.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:point_of_sales_cashier/common/widgets/appbar/custom_appbar.dart';
+import 'package:point_of_sales_cashier/common/widgets/ui/bottomsheet/general_information.dart';
 import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_action_l.dart';
 import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_body_l.dart';
-import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_body_m.dart';
 import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_heading_2.dart';
 import 'package:point_of_sales_cashier/utils/constants/colors.dart';
 import 'package:point_of_sales_cashier/utils/constants/error_text_strings.dart';
@@ -75,7 +75,7 @@ class _EmailEditScreenState extends State<EmailEditScreen> {
                 showModalBottomSheet(
                   context: context,
                   builder: (context) {
-                    return InformationBottomSheet(
+                    return GeneralInformation(
                       imageSrc: TImages.generalIllustration,
                       title: "Cek email kamu untuk verifikasi",
                       description:
@@ -85,9 +85,9 @@ class _EmailEditScreenState extends State<EmailEditScreen> {
                         showModalBottomSheet(
                           context: context,
                           builder: (context) {
-                            return InformationBottomSheet(
+                            return GeneralInformation(
                               imageSrc: TImages.successIllustration,
-                              title: "Berhasil verifikasi email baru ",
+                              title: "Berhasil verifikasi email baru",
                               description:
                                   "Email kamu berhasil terverifikasi dan aktif untuk bisa digunakan.",
                               onRequest: () {
@@ -107,79 +107,6 @@ class _EmailEditScreenState extends State<EmailEditScreen> {
               child: const TextActionL(
                 "Lanjut Verifikasi Email",
               ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class InformationBottomSheet extends StatelessWidget {
-  const InformationBottomSheet({
-    super.key,
-    required this.description,
-    required this.imageSrc,
-    required this.title,
-    required this.onRequest,
-    this.requestTitle,
-  });
-
-  final String imageSrc;
-  final String title;
-  final String description;
-  final Function() onRequest;
-  final String? requestTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      width: double.infinity,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 20),
-                  child: Image.asset(
-                    imageSrc,
-                    height: 72.46,
-                    width: 100,
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 8),
-                  child: TextHeading2(
-                    title,
-                    color: TColors.neutralDarkDarkest,
-                  ),
-                ),
-                TextBodyM(
-                  description,
-                  color: TColors.neutralDarkMedium,
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 48,
-                    child: ElevatedButton(
-                      onPressed: onRequest,
-                      child: TextActionL(requestTitle ?? "Berikan Izin"),
-                    ),
-                  ),
-                ),
-              ],
             ),
           ),
         ],
