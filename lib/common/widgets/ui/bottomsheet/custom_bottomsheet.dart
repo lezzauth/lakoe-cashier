bdc67f606584c10628ad263b3943e1c1b06ed5d1
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-09-20 20:46:56 +0700

    fix bill issue parentwidget

---
+++ lib/common/widgets/ui/bottomsheet/custom_bottomsheet.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:point_of_sales_cashier/common/widgets/ui/bottomsheet/grabber.dart';
+
+class CustomBottomsheet extends StatelessWidget {
+  const CustomBottomsheet({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Grabber(),
+        child,
+      ],
+    );
+  }
+}
