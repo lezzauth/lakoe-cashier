7f90a29ab77d7af0b11ae7104da04225db638f2b
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-09-21 18:37:04 +0700

    bottomsheet logout confirmation

--- lib/common/widgets/ui/bottomsheet/custom_bottomsheet.dart
+++ lib/common/widgets/ui/bottomsheet/custom_bottomsheet.dart
@@ -9,6 +9,7 @@ class CustomBottomsheet extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.min,
       children: [
         const Grabber(),
