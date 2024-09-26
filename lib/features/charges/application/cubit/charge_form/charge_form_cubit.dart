6939b95ae535f4ed4d1bb358c0d038fefc24ef59
Ilham Adiputra <ilhamadiputra4@gmail.com>
2024-09-26 21:14:06 +0700

    feat(charge-master): prevent form cancel

---
+++ lib/features/charges/application/cubit/charge_form/charge_form_cubit.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:point_of_sales_cashier/features/charges/application/cubit/charge_form/charge_form_state.dart';
+import 'package:point_of_sales_cashier/features/charges/presentation/widgets/form/field/other_charge_field.dart';
+
+class ChargeFormCubit extends Cubit<ChargeFormState> {
+  ChargeFormCubit()
+      : super(
+          const ChargeFormState(
+            charges: [],
+            value: <String, dynamic>{},
+            chargeCount: 0,
+            isFormValid: false,
+          ),
+        );
+
+  void setValue(Map<String, dynamic>? value, bool isFormValid) {
+    emit(ChargeFormState(
+      charges: state.charges,
+      value: value ?? state.value,
+      chargeCount: state.chargeCount,
+      isFormValid: isFormValid,
+    ));
+  }
+
+  void addCharge(OtherChargeField tax) {
+    final int newChargeCount = state.chargeCount + 1;
+    emit(ChargeFormState(
+      charges: [...state.charges, tax],
+      value: state.value,
+      chargeCount: newChargeCount,
+    ));
+  }
+
+  void removeCharge(ValueKey key) {
+    final List<OtherChargeField> currentCharges = List.from(state.charges);
+    currentCharges.removeWhere((e) => e.key == key);
+
+    emit(ChargeFormState(
+        charges: currentCharges,
+        value: state.value,
+        chargeCount: state.chargeCount));
+  }
+}
