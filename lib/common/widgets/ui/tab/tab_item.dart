a354b47f7eda29775fba0f0f06b8e9ec1db10620
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-09-27 20:25:52 +0700

    ui package

--- lib/common/widgets/ui/tab/tab_item.dart
+++ lib/common/widgets/ui/tab/tab_item.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_caption_m.dart';
 import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_heading_4.dart';
 import 'package:point_of_sales_cashier/utils/constants/colors.dart';
+import 'package:point_of_sales_cashier/utils/constants/sizes.dart';
 
 class TabItem extends StatelessWidget {
-  const TabItem({super.key, required this.title, this.counter});
+  const TabItem({
+    super.key,
+    required this.title,
+    this.counter,
+    this.fontSize = TSizes.fontSizeHeading4,
+    this.labelAssets,
+  });
 
   final String title;
   final int? counter;
+  final double fontSize;
+  final Widget? labelAssets;
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +25,30 @@ class TabItem extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(child: TextHeading4(title)),
+            Flexible(
+              child: labelAssets ??
+                  TextHeading4(
+                    title,
+                    textAlign: TextAlign.center,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    fontSize: fontSize,
+                  ),
+            ),
             if (counter != null) ...[
               const SizedBox(width: 8),
               Container(
-                height: 16,
-                width: 16,
+                height: 20,
+                width: 20,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
+                  borderRadius: BorderRadius.circular(20),
                   color: TColors.error,
                 ),
                 child: Center(
                   child: TextCaptionM(
                     counter!.toString(),
                     color: TColors.neutralLightLightest,
+                    textAlign: TextAlign.center,
                   ),
                 ),
               )
---
+++ lib/features/packages/presentation/screens/package_master.dart
@@ -0,0 +1,309 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:point_of_sales_cashier/common/widgets/icon/ui_icons.dart';
+import 'package:point_of_sales_cashier/common/widgets/ui/tab/tab_container.dart';
+import 'package:point_of_sales_cashier/common/widgets/ui/tab/tab_item.dart';
+import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_action_l.dart';
+import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_body_m.dart';
+import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_heading_2.dart';
+import 'package:point_of_sales_cashier/common/widgets/ui/typography/text_heading_5.dart';
+import 'package:point_of_sales_cashier/utils/constants/colors.dart';
+import 'package:point_of_sales_cashier/utils/constants/icon_strings.dart';
+import 'package:point_of_sales_cashier/utils/constants/image_strings.dart';
+import 'package:point_of_sales_cashier/utils/constants/sizes.dart';
+import 'package:point_of_sales_cashier/utils/formatters/formatter.dart';
+
+class PackageMasterScreen extends StatefulWidget {
+  const PackageMasterScreen({super.key});
+
+  @override
+  State<PackageMasterScreen> createState() => _PackageMasterScreenState();
+}
+
+class _PackageMasterScreenState extends State<PackageMasterScreen>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+  int _selectedIndex = 0;
+
+  List<_CardItemPackage> listCardItem = [
+    _CardItemPackage(
+      logo: TImages.liteLogoPackage,
+      description:
+          "Untuk bisnis baru, fitur lengkap dengan batasan penggunaan.",
+      price: 0,
+      color: Color(0xFFFC4100),
+      isActive: true,
+    ),
+    _CardItemPackage(
+      logo: TImages.growLogoPackage,
+      description: "Tingkatkan kapasitas bisnis dengan kuota yang lebih besar",
+      price: 30000,
+      color: Color(0xFF00712D),
+      isActive: false,
+    ),
+    _CardItemPackage(
+      logo: TImages.proLogoPackage,
+      description:
+          "Bebaskan potensi bisnis dengan akses tanpa terbatas ke semua fitur Lakoe.",
+      price: 56000,
+      color: Color(0xFF9306AF),
+      isActive: false,
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController?.addListener(() {
+      setState(() {
+        _selectedIndex = _tabController!.index;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: PackageAppbar(),
+      body: Stack(
+        children: [
+          Positioned(
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: SvgPicture.asset(
+                TImages.package,
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 112),
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextHeading2("Saatnya Naik Level,\nSaatnya Upgrade Lakoe."),
+                SizedBox(height: 8),
+                TabContainer(
+                  hasPadding: true,
+                  controller: _tabController,
+                  tabs: [
+                    const TabItem(
+                      title: "Paket",
+                      fontSize: TSizes.fontSizeHeading3,
+                    ),
+                    TabItem(
+                      title: "Boost",
+                      labelAssets: _selectedIndex != 1
+                          ? SvgPicture.asset(
+                              TImages.boostLogo,
+                              height: 16,
+                              width: 62,
+                            )
+                          : SvgPicture.asset(
+                              TImages.boostLogoLight,
+                              height: 16,
+                              width: 62,
+                            ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      PackageTabView(children: listCardItem),
+                      BoostTabView(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PackageTabView extends StatelessWidget {
+  const PackageTabView({
+    super.key,
+    required this.children,
+  });
+
+  final List children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 12),
+      child: Column(
+        children: children
+            .map((item) => Container(
+                  margin: EdgeInsets.only(bottom: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16),
+                    border: Border.all(
+                      color: TColors.neutralLightMedium,
+                      width: 1,
+                    ),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        top: 0,
+                        right: 0,
+                        child: SvgPicture.asset(
+                          TImages.pakcageWaves,
+                          colorFilter: ColorFilter.mode(
+                            item.color,
+                            BlendMode.srcIn,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Image.asset(
+                              item.logo,
+                              height: 28,
+                            ),
+                            SizedBox(height: 12),
+                            TextBodyM(
+                              item.description,
+                              color: TColors.neutralDarkLight,
+                            ),
+                            SizedBox(height: 12),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    if (item.price != 0)
+                                      TextHeading5(
+                                        "Mulai dari",
+                                        color: TColors.neutralDarkLightest,
+                                      ),
+                                    TextHeading2(
+                                      item.price == 0
+                                          ? "Gratis"
+                                          : TFormatter.formatToRupiah(
+                                              item.price),
+                                      color: TColors.neutralDarkDark,
+                                    ),
+                                  ],
+                                ),
+                                if (item.isActive == false)
+                                  ElevatedButton(
+                                    onPressed: () {},
+                                    style: ButtonStyle(
+                                      minimumSize: WidgetStateProperty.all(
+                                        Size(0, 36),
+                                      ),
+                                      padding: WidgetStateProperty.all(
+                                        EdgeInsets.symmetric(
+                                          horizontal: 16,
+                                          vertical: 8,
+                                        ),
+                                      ),
+                                    ),
+                                    child: Text("Cek Detail"),
+                                  ),
+                                if (item.isActive == true)
+                                  Container(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8, vertical: 4),
+                                    decoration: BoxDecoration(
+                                      color: TColors.neutralLightMedium,
+                                      borderRadius: BorderRadius.circular(4),
+                                    ),
+                                    child: const TextHeading5(
+                                      "Paket kamu saat ini",
+                                      color: TColors.neutralDarkDark,
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class BoostTabView extends StatelessWidget {
+  const BoostTabView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(top: 12),
+      child: Placeholder(),
+    );
+  }
+}
+
+class PackageAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const PackageAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 18),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SvgPicture.asset(
+            TImages.primaryLogoLakoe,
+            height: 20,
+            width: 77.1,
+          ),
+          UiIcons(
+            onTap: () => Navigator.pop(context),
+            TIcons.close,
+            color: TColors.neutralDarkLightest,
+          ),
+        ],
+      ),
+    ));
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(68.0);
+}
+
+class _CardItemPackage {
+  final String logo;
+  final String description;
+  final bool isActive;
+  final int price;
+  final Color color;
+
+  _CardItemPackage({
+    required this.logo,
+    required this.description,
+    this.isActive = false,
+    required this.price,
+    required this.color,
+  });
+}
