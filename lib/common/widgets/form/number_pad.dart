55b13c5062e3e08658c1fd5ac730d05d4d09e0fc
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-10-01 11:51:45 +0700

    minor changes

--- lib/common/widgets/form/number_pad.dart
+++ lib/common/widgets/form/number_pad.dart
@@ -44,157 +44,164 @@ class _NumberPadState extends State<NumberPad> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Wrap(
-          spacing: 8.0,
-          children: [
-            Pad(
-                onTap: () {
-                  handleChange("1");
-                },
-                child: Text("1",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-            Pad(
-                onTap: () {
-                  handleChange("2");
-                },
-                child: Text("2",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-            Pad(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          Wrap(
+            crossAxisAlignment: WrapCrossAlignment.center,
+            alignment: WrapAlignment.center,
+            runAlignment: WrapAlignment.center,
+            spacing: 12.0,
+            runSpacing: 4.0,
+            children: [
+              Pad(
+                  onTap: () {
+                    handleChange("1");
+                  },
+                  child: Text("1",
+                      style: GoogleFonts.inter(
+                        fontSize: TSizes.fontSizeHeading1,
+                        fontWeight: FontWeight.w900,
+                        color: TColors.neutralDarkDarkest,
+                      ))),
+              Pad(
+                  onTap: () {
+                    handleChange("2");
+                  },
+                  child: Text("2",
+                      style: GoogleFonts.inter(
+                        fontSize: TSizes.fontSizeHeading1,
+                        fontWeight: FontWeight.w900,
+                        color: TColors.neutralDarkDarkest,
+                      ))),
+              Pad(
                 onTap: () {
                   handleChange("3");
                 },
-                child: Text("3",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-          ],
-        ),
-        Wrap(
-          spacing: 8.0,
-          children: [
-            Pad(
-                onTap: () {
-                  handleChange("4");
-                },
-                child: Text("4",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-            Pad(
-                onTap: () {
-                  handleChange("5");
-                },
-                child: Text("5",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-            Pad(
+                child: Text(
+                  "3",
+                  style: GoogleFonts.inter(
+                    fontSize: TSizes.fontSizeHeading1,
+                    fontWeight: FontWeight.w900,
+                    color: TColors.neutralDarkDarkest,
+                  ),
+                ),
+              ),
+
+              // New Row
+              Pad(
+                  onTap: () {
+                    handleChange("4");
+                  },
+                  child: Text("4",
+                      style: GoogleFonts.inter(
+                        fontSize: TSizes.fontSizeHeading1,
+                        fontWeight: FontWeight.w900,
+                        color: TColors.neutralDarkDarkest,
+                      ))),
+              Pad(
+                  onTap: () {
+                    handleChange("5");
+                  },
+                  child: Text("5",
+                      style: GoogleFonts.inter(
+                        fontSize: TSizes.fontSizeHeading1,
+                        fontWeight: FontWeight.w900,
+                        color: TColors.neutralDarkDarkest,
+                      ))),
+              Pad(
                 onTap: () {
                   handleChange("6");
                 },
-                child: Text("6",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-          ],
-        ),
-        Wrap(
-          spacing: 8.0,
-          children: [
-            Pad(
-                onTap: () {
-                  handleChange("7");
-                },
-                child: Text("7",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-            Pad(
-                onTap: () {
-                  handleChange("8");
-                },
-                child: Text("8",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-            Pad(
+                child: Text(
+                  "6",
+                  style: GoogleFonts.inter(
+                    fontSize: TSizes.fontSizeHeading1,
+                    fontWeight: FontWeight.w900,
+                    color: TColors.neutralDarkDarkest,
+                  ),
+                ),
+              ),
+
+              // New row
+              Pad(
+                  onTap: () {
+                    handleChange("7");
+                  },
+                  child: Text("7",
+                      style: GoogleFonts.inter(
+                        fontSize: TSizes.fontSizeHeading1,
+                        fontWeight: FontWeight.w900,
+                        color: TColors.neutralDarkDarkest,
+                      ))),
+              Pad(
+                  onTap: () {
+                    handleChange("8");
+                  },
+                  child: Text("8",
+                      style: GoogleFonts.inter(
+                        fontSize: TSizes.fontSizeHeading1,
+                        fontWeight: FontWeight.w900,
+                        color: TColors.neutralDarkDarkest,
+                      ))),
+              Pad(
                 onTap: () {
                   handleChange("9");
                 },
-                child: Text("9",
-                    style: GoogleFonts.inter(
-                      fontSize: TSizes.fontSizeHeading1,
-                      fontWeight: FontWeight.w900,
-                      color: TColors.neutralDarkDarkest,
-                    ))),
-          ],
-        ),
-        Wrap(
-          spacing: 8.0,
-          children: [
-            Pad(
-              onTap: () {
-                Navigator.pop(context);
-              },
-              color: Colors.transparent,
-              child: Text(
-                "Batal",
-                style: GoogleFonts.inter(
-                  fontSize: TSizes.fontSizeHeading4,
-                  fontWeight: FontWeight.w600,
-                  color: TColors.primary,
+                child: Text(
+                  "9",
+                  style: GoogleFonts.inter(
+                    fontSize: TSizes.fontSizeHeading1,
+                    fontWeight: FontWeight.w900,
+                    color: TColors.neutralDarkDarkest,
+                  ),
                 ),
               ),
-            ),
-            Pad(
-              onTap: () {
-                handleChange("0");
-              },
-              child: Text(
-                "0",
-                style: GoogleFonts.inter(
-                  fontSize: TSizes.fontSizeHeading1,
-                  fontWeight: FontWeight.w900,
+
+              //New row
+              Pad(
+                onTap: () {
+                  // Navigator.pop(context);
+                },
+                color: Colors.transparent,
+                child: Text(
+                  "Lupa?",
+                  style: GoogleFonts.inter(
+                    fontSize: TSizes.fontSizeHeading4,
+                    fontWeight: FontWeight.w600,
+                    color: TColors.primary,
+                  ),
+                ),
+              ),
+              Pad(
+                onTap: () {
+                  handleChange("0");
+                },
+                child: Text(
+                  "0",
+                  style: GoogleFonts.inter(
+                    fontSize: TSizes.fontSizeHeading1,
+                    fontWeight: FontWeight.w900,
+                    color: TColors.neutralDarkDarkest,
+                  ),
+                ),
+              ),
+              Pad(
+                onTap: () {
+                  handleBackspace();
+                },
+                child: const UiIcons(
+                  TIcons.backspace,
+                  width: 24,
+                  height: 24,
                   color: TColors.neutralDarkDarkest,
                 ),
               ),
-            ),
-            Pad(
-              onTap: () {
-                handleBackspace();
-              },
-              child: const UiIcons(
-                TIcons.backspace,
-                width: 24,
-                height: 24,
-                color: TColors.neutralDarkDarkest,
-              ),
-            ),
-          ],
-        ),
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
