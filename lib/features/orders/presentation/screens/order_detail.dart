476c26d72875158356c0516eecd0847ed53fb068
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-10-01 21:44:36 +0700

    cosmetic issue

--- lib/features/orders/presentation/screens/order_detail.dart
+++ lib/features/orders/presentation/screens/order_detail.dart
@@ -319,56 +319,59 @@ class _OrderDetailState extends State<OrderDetail> {
           child: Expanded(
             child: Column(
               children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.only(bottom: 16),
-                        child: const TextHeading2("Tampilan Struk"),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                      decoration: const BoxDecoration(
+                        border: Border(
+                          bottom: BorderSide(
+                            color: TColors.neutralLightMedium,
+                            width: 1.0,
+                          ),
+                        ),
                       ),
-                    ],
-                  ),
+                      child: const TextHeading2("Tampilan Struk"),
+                    ),
+                  ],
                 ),
                 Expanded(
-                  child: SingleChildScrollView(
-                    controller: scrollController,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Container(
-                        color: TColors.neutralLightLight,
-                        child: RepaintBoundary(
-                          key: _billWidgetKey,
-                          child: Column(
-                            children: [
-                              BillView(
-                                outletName: "Warmindo Cak Tho",
-                                outletAddress:
-                                    "Tebet,Jakarta Selatan, DKI Jakarta",
-                                orderNumber: "9849",
-                                cashierName: "Dimas",
-                                noBill: "LK-0001",
-                                orderType: "Take Away",
-                                dateTime: "28/12/2024, 20:18",
-                                paymentMetod: 'Cash (Tunai)',
-                                totalPrice: "Rp21.400",
-                                moneyReceived: "Rp50.000",
-                                changeMoney: "Rp28.600",
-                                closeBill: "Close Bill: 28/12/2024, 21:37",
-                                greeting:
-                                    "Terimakasih\nDitunggu kembali kedatangannya",
-                                children: listBillPriceItem
-                                    .map(
-                                      (item) => BillListPrice(
-                                        label: item.label,
-                                        price: item.price,
-                                      ),
-                                    )
-                                    .toList(),
-                              ),
-                            ],
-                          ),
+                  child: Container(
+                    color: TColors.neutralLightLight,
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: SingleChildScrollView(
+                      controller: scrollController,
+                      child: RepaintBoundary(
+                        key: _billWidgetKey,
+                        child: Column(
+                          children: [
+                            BillView(
+                              outletName: "Warmindo Cak Tho",
+                              outletAddress:
+                                  "Tebet,Jakarta Selatan, DKI Jakarta",
+                              orderNumber: "9849",
+                              cashierName: "Dimas",
+                              noBill: "LK-0001",
+                              orderType: "Take Away",
+                              dateTime: "28/12/2024, 20:18",
+                              paymentMetod: 'Cash (Tunai)',
+                              totalPrice: "Rp21.400",
+                              moneyReceived: "Rp50.000",
+                              changeMoney: "Rp28.600",
+                              closeBill: "Close Bill: 28/12/2024, 21:37",
+                              greeting:
+                                  "Terimakasih\nDitunggu kembali kedatangannya",
+                              children: listBillPriceItem
+                                  .map(
+                                    (item) => BillListPrice(
+                                      label: item.label,
+                                      price: item.price,
+                                    ),
+                                  )
+                                  .toList(),
+                            ),
+                          ],
                         ),
                       ),
                     ),
