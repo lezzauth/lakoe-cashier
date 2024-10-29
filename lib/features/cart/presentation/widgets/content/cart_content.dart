2d6e357a4befbe48a1541d87923c9767ec7b9c72
Muhammad Najib <muhammad.es.najib@gmail.com>
2024-10-29 19:14:44 +0700

    consistency label

--- lib/features/cart/presentation/widgets/content/cart_content.dart
+++ lib/features/cart/presentation/widgets/content/cart_content.dart
@@ -254,8 +254,23 @@ class _CartContentState extends State<CartContent> {
                       ),
                       SliverToBoxAdapter(
                         child: Container(
-                          margin: const EdgeInsets.symmetric(vertical: 28.0),
-                          child: const PreviewOrderPrice(),
+                          margin: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(
+                                  bottom: 12.0,
+                                  top: 4,
+                                ),
+                                child: const TextHeading3(
+                                  "Ringkasan tagihan",
+                                  color: TColors.neutralDarkDarkest,
+                                ),
+                              ),
+                              const PreviewOrderPrice(),
+                            ],
+                          ),
                         ),
                       ),
                     ],
